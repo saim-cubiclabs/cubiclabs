eb36b6535d4e1f0d545f312f1226a83f92fe88b7
saim-cubiclabs <saim@cubiclabs.net>
2025-10-27 14:57:58 +0300

    added new ui component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,14 @@ import { StatsSection } from "@/components/stats-section";
 import { ServicesSection } from "@/components/services-section";
 import { CTASection } from "@/components/cta-section";
 import { Skiper31 } from "@/components/ui/skiper-ui/skiper31";
+import { HeroParallaxDemo } from "@/components/heroparallaxdemo";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
       <StatsSection />
+      <HeroParallaxDemo />
       <ServicesSection />
       <Skiper31 />
       <CTASection />
---
+++ src/components/heroparallaxdemo.jsx
@@ -0,0 +1,85 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+
+export function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
+}
+
+export const products = [
+  {
+    title: "Cubic Labs Dashboard",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/dashboard.png",
+  },
+  {
+    title: "E-commerce Platform",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/ecom.jpg",
+  },
+  {
+    title: "Business Website",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/website.jpg",
+  },
+  {
+    title: "Car Rental System",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/car.png",
+  },
+  {
+    title: "Optimization Tools",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/optimization.png",
+  },
+  {
+    title: "Armada Project",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/armada-removebg-preview.png",
+  },
+  {
+    title: "M&S Solutions",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/m&s.jpg",
+  },
+  {
+    title: "About Page Design",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/about1.png",
+  },
+  {
+    title: "About Section",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/about2.png",
+  },
+  {
+    title: "Cubic Labs Dashboard",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/dashboard.png",
+  },
+  {
+    title: "E-commerce Platform",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/ecom.jpg",
+  },
+  {
+    title: "Business Website",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/website.jpg",
+  },
+  {
+    title: "Car Rental System",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/car.png",
+  },
+  {
+    title: "Optimization Tools",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/optimization.png",
+  },
+  {
+    title: "Growth Analytics",
+    link: "https://cubiclabs.vercel.app/",
+    thumbnail: "/up.png",
+  },
+];
